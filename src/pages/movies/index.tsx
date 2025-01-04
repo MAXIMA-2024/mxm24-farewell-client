eb877d8edf6589a546b73587dbefb035daad7a01
jun <juntesemail@gmail.com>
2025-01-04 21:49:12 +0700

    feat: background image + layout

--- src/pages/movies/index.tsx
+++ src/pages/movies/index.tsx
@@ -2,7 +2,21 @@ import { Stack } from "@chakra-ui/react";
 
 const MoviesPage = () => {
   return (
-    <Stack>
+    <Stack
+      bgColor={"red"}
+      minW={"100vw"}
+      minH={"100vh"}
+      backgroundImage={[
+        `url(/bg/galleryBG4_teater_mobile.png)`,
+        `url(/bg/galleryBG4_teater_mobile.png)`,
+        `url(/bg/galleryBG4_teater.png)`,
+        `url(/bg/galleryBG4_teater.png)`,
+        `url(/bg/galleryBG4_teater.png)`,
+      ]}
+      backgroundSize={"cover"}
+      backgroundPosition={"top"}
+      backgroundRepeat={"no-repeat"}
+    >
       <p>Movie Page</p>
     </Stack>
   );
