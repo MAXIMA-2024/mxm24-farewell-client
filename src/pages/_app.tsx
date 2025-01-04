f98face29f324f73013db4ccfc12ddf8e103a101
AlbertTech23 <alberttirtokusumo@gmail.com>
2025-01-04 22:46:27 +0700

    fix: add overflow on layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
-    <Stack w={"100vw"} minH={"100vh"}>
+    <Stack w={"100vw"} minH={"100vh"} overflow={"hidden"} position={"relative"}>
       <Outlet />
     </Stack>
   );
